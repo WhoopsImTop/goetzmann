59fe68a399da412682a154c5d38ce5cde85f44fa
Elias4711 <elias.englen@geops.de>
2023-02-08 17:44:30 +0100

    add new

--- components/footerComponent.vue
+++ components/footerComponent.vue
@@ -1,18 +1,26 @@
 <template>
   <div class="footer">
-    <div class="content-container">
+    <div class="content-container footer-row">
+      <div>
         <nuxt-link to="impressum">Impressum</nuxt-link>
         <nuxt-link to="datenschutz">Datenschutz</nuxt-link>
+      </div>
+      <div class="socialIcons">
+        <img
+          src="img/in_w.svg"
+          width="20"
+          style="margin-right: 10px"
+          alt="instagram"
+        />
+        <img src="img/fb_w.svg" width="10" alt="facebook" />
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-
 </style>
--- mail.php
+++ mail.php
@@ -3,7 +3,17 @@
 $name = $_POST['name'];
 $email = $_POST['email'];
 $subject = $_POST['subject'];
-$message = $_POST['message'];
+$message = $_POST['nachricht'];
 
+$message_content = "<p>Von: $name</p>";
+$message_content .= "<p>E-Mail: $email</p>";
+$message_content .= "<p>Betreff: $subject</p>";
+$message_content .= "<p>Nachricht: $message</p>";
 
-?>
+$subject = "Kontaktformular OB Wahl 2023: $subject";
+$headers[] = 'MIME-Version: 1.0';
+$headers[] = 'Content-type: text/html; charset=utf-8';
+$headers[] = 'From:' . $name . '<' . strval($email) . '>';
+$headers[] = 'roman-goetzmann.de';
+
+mail('eliasenglen2@gmail.com', $subject, $message_content, implode("\r\n", $headers));
